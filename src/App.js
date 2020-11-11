c0d17e48150a0c37493bdb6251900d0313422ed6
Allyson Athyrson <athyrsonallyson@gmail.com>
2020-11-10 22:36:11 -0300

    criação do navbar responsivo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './styles/global.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Jasph está on!</h1>
-    </div>
-  );
+import './styles/app.css';
+import NavLogo from './assets/logoJasph.png';
+
+export default class App extends React.Component {
+  navResponsive(){
+    let navItems = document.getElementById('nav-items');
+    navItems.classList.toggle("responsive");
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="header">
+          <div className="nav-logo">
+            <img src={NavLogo} className="imageLogo" alt="logo Jasph" />
+          </div>
+          <div className="nav-items" id="nav-items">
+            <a href="#" id="contato" className="contato">contato</a>
+            <a href="#" id="sobreNos" className="sobreNos">sobre nós</a>
+            <a href="#" id="integrantes" className="integrantes">integrantes</a>
+            <a href="#" id="portfolio" className="portfolio">portfólio</a>
+            <a href="#" id="organizacao" className="organizacao">organização</a>
+            <a href="#" id="tecnologias" className="tecnologias">tecnologias</a>
+          </div>
+          <a href="javascript:void(0);" className="nav-close" onClick={this.navResponsive}>
+            <i class="fa fa-bars"></i>
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
